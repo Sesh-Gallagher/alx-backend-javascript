9bbb71ccb9939a115d069cd7235e3591d914d9a5
Sesh-Gallagher <t.ntwana@gmail.com>
2024-10-09 13:35:52 +0000

    more mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(students) {
-  if (students instanceof Array) {
+  if (students instanceof Array)
     return students.map((student) => student.id);
   }
   return [];
