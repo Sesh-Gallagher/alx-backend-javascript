012d1862837340d1d32bafa4d3e4f7b43c0a8a28
Sesh-Gallagher <t.ntwana@gmail.com>
2024-10-09 14:48:20 +0000

    100- await fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,7 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   let res = {};
-
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
