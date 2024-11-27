c54ffe8cf24a670146d8e090b04bef4ddbfbd4cb
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-27 02:52:24 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+/*
+ * Function to Install Express and in a file named 6-http_express.js
+ */
+
+const express = require('express');
+const app = express();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
