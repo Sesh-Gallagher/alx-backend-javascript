c365e01dfd5ccba896427c0ce22770b0c58c80b5
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-29 20:57:39 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,6 @@
 /**
  * Function counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * 
  */
 
 const fs = require('fs');
@@ -18,7 +17,7 @@ const countStudents = (filePath) => {
     .toString('utf-8')
     .trim()
     .split('\n');
-  const studentGroups = {};
+  const studentgroups = {};
   const dbFieldNames = fileLines[0].split(',');
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
@@ -26,19 +25,19 @@ const countStudents = (filePath) => {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+    if (!Object.keys(studentgroups).includes(field)) {
+      studentgroups[field] = [];
     }
     const studentEntries = studentPropNames
       .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
+    studentgroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const total_Students = Object
-    .values(studentGroups)
+  const total_students = Object
+    .values(studentgroups)
     .reduce((pre, cur) => (pre || []).length + cur.length);
-  console.log(`Number of students: ${total_Students}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
+  console.log(`Number of students: ${total_students}`);
+  for (const [field, group] of Object.entries(studentgroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
