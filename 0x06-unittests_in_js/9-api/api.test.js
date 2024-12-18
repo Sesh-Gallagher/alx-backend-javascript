24e2f777f0e695e9445b79ee9f95ca99cee1b528
Sesh-Gallagher <t.ntwana@gmail.com>
2024-12-18 11:49:30 +0000

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,48 @@
+/**
+ * Test for App created
+ */
+const request = require('request');
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Index page', () => {
+  it('response', (done) => {
+    try {
+      request('http://localhost:7865/', (err, res, body) => {
+        if (err) throw err;
+        expect(body).to.be.equal('Welcome to the payment system');
+        expect(res.statusCode).to.be.equal(200);
+        done();
+      });
+    } catch (err) {
+      done(err);
+    }
+  });
+});
+describe('cart', () => {
+    it('response', (done) => {
+      try {
+        request('http://localhost:7865/cart/7', (error, response, body) => {
+          if (error) throw error;
+          expect(body).to.equal('Payment methods for cart 7');
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
+    });
+    it('not number', (done) => {
+      try {
+        request('http://localhost:7865/cart/devops', (error, response) => {
+          if (error) throw error;
+          expect(response.statusCode).to.equal(404);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
+});
